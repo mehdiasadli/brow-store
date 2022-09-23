7c3643146bda145485ab45739f625c0a5dc6c407
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2022-09-23 16:09:01 +0400

    feat: added more functions for localStorage

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,96 @@
 export const setLocal = (key: string, value: any) => {
-  localStorage.setItem(key, JSON.stringify(value));
+  localStorage.setItem(key, JSON.stringify(value))
 };
 
-export const getLocal = (key: string, defaultValue: any = null): any => {
-  const result = localStorage.getItem(key);
-  if (!result) return defaultValue;
-  return JSON.parse(result);
+export const getLocal = (key: string, defaultValue = null): any => {
+  const result = localStorage.getItem(key)
+  if (!result) return defaultValue
+
+  return JSON.parse(result)
 };
+
+export const removeLocal = (key: string) => {
+    localStorage.removeItem(key)
+}
+
+export const clearLocal = () => {
+    localStorage.clear()
+}
+
+export const getLocalLength = (): number => localStorage.length
+
+export const getLocalKey = (index: number, defaultValue: any = null, log = false): string | null => {
+    const result = localStorage.key(index) || defaultValue
+    if(log) console.log('KEY: ', result)
+
+    return result
+}
+
+export const getLocalValue = (index: number, defaultKey: any = null, log = false) => {
+    const key = getLocalKey(index)
+    if(!key) {
+        if(log) console.log('VALUE: ', defaultKey)
+        return defaultKey
+    }
+
+    const result = getLocal(key)
+
+    if (log) console.log('VALUE: ', result)
+    return result
+}
+
+export const getLocalKeys = (log = false): string[] => {
+    let result: string[] = []
+    const length = getLocalLength()
+
+    for(let i = 0; i < length; i++) {
+        const key = getLocalKey(i)
+        if(key) {
+            result.push(key)
+        }
+
+        if(log) {
+            console.log(`KEY ${i + 1}: ${key} `)
+        }
+    }
+
+    return result
+}
+
+export const getLocalValues = (log = false): any[] => {
+    let result: any[] = []
+    const length = getLocalLength()
+
+    for(let i = 0; i < length; i++) {
+        const value = getLocalValue(i)
+        if(value) {
+            result.push(value)
+        }
+
+        if(log) {
+            console.log(`VALUE ${i + 1}: ${value} `)
+        }
+    }
+
+    return result
+}
+
+interface LocalObject {
+    [key: string]: any
+}
+
+export const getAllLocal = () => {
+    let result: LocalObject = {}
+    const length = getLocalLength()
+
+    for(let i = 0; i < length; i++) {
+        const key = getLocalKey(i)
+        const value = getLocalValue(i)
+
+        if(key && value) {
+            result[key] = value
+        }
+    }
+
+    return result
+}
