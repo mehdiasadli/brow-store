3aa9b86e161735450d50df2fa31ca7206755b99f
Mehdi Asadli <mehdiasadli@Mehdis-MacBook-Pro.local>
2022-09-23 16:11:26 +0400

    fix: added any type to getLocal defaultValue param

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ export const setLocal = (key: string, value: any) => {
   localStorage.setItem(key, JSON.stringify(value))
 };
 
-export const getLocal = (key: string, defaultValue = null): any => {
+export const getLocal = (key: string, defaultValue: any = null): any => {
   const result = localStorage.getItem(key)
   if (!result) return defaultValue
 
